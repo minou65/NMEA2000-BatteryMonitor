7e2b2d2edfec6e46fafac4ad827d2aefd67149a8
Andy <azogg65@bluewin.ch>
2023-12-26 07:24:28 +0100

    file renamed

---
+++ src/NMEA2000-BatteryMonitor.ino
@@ -0,0 +1,60 @@
+#include <Arduino.h>
+
+#include "common.h"
+#include "StatusHandling.h"
+#include "SensorHandling.h"
+#include "webHandling.h"
+#include "TemperaturHandling.h"
+#include "NMEAHandling.h"
+
+char Version[] = "0.0.0.2 (2023-07-31)"; // Manufacturer's Software version code
+
+// # define IOTWEBCONF_DEBUG_TO_SERIAL true
+
+// Task handle (Core 0 on ESP32)
+TaskHandle_t TaskHandle;
+
+void setup() {
+    Serial.begin(115200);
+
+    // wait for serial port to open on native usb devices
+    while (!Serial) {
+        delay(1);
+    }
+
+    wifiSetup();
+
+    sensorInit();
+    TemperaturInit();
+    N2kInit();
+
+    xTaskCreatePinnedToCore(
+        loop2, /* Function to implement the task */
+        "TaskHandle", /* Name of the task */
+        10000,  /* Stack size in words */
+        NULL,  /* Task input parameter */
+        0,  /* Priority of the task */
+        &TaskHandle,  /* Task handle. */
+        0 /* Core where the task should run */
+    );
+}
+
+void loop() {
+    if (gParamsChanged) {
+
+    }
+    wifiLoop();
+    sensorLoop();
+    N2Kloop();
+
+    gParamsChanged = false;
+}
+
+void loop2(void* parameter) {
+    for (;;) {   // Endless loop
+
+        TemperaturLoop();
+        vTaskDelay(100);
+
+    }
+}
