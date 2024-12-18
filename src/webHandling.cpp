d5a64b9101e218d5e2da85ddd43db36a70342e84
Andy <azogg65@bluewin.ch>
2024-12-18 20:48:08 +0100

    Fix platform detection in webHandling.cpp for ESP32 and ESP8266

--- src/webHandling.cpp
+++ src/webHandling.cpp
@@ -4,12 +4,13 @@
 
 #include <Arduino.h>
 #include <ArduinoOTA.h>
-#if ESP32
+#if defined(ESP32)
 #include <WiFi.h>
 // #include <esp_wifi.h>
- #else
-#include <ESP8266WiFi.h>   
-
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#else
+#error "Unsupported platform"
 #endif
 
 #include "common.h"
