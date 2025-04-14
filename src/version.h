fcbbd7ad7e04d4691d936cb97e30218307742dc7
Andy <azogg65@bluewin.ch>
2025-04-14 07:10:40 +0200

    Update version to 1.3.13.259 with new build date and time

--- src/version.h
+++ src/version.h
@@ -3,8 +3,8 @@
 #define VERSION_PATCH 13
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 258
-#define VERSION_DATE "2024-12-18"
-#define VERSION_TIME "20:46:51"
-#define VERSION "1.3.13.258"
-#define VERSION_STR "1.3.13.258 (2024-12-18 20:46:51)"
+#define VERSION_BUILD 259
+#define VERSION_DATE "2025-04-14"
+#define VERSION_TIME "07:06:58"
+#define VERSION "1.3.13.259"
+#define VERSION_STR "1.3.13.259 (2025-04-14 07:06:58)"
