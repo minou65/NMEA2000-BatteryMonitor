4127367729d97013d692ab520d92de1d3194d3e3
Andy <azogg65@bluewin.ch>
2026-04-26 08:29:00 +0200

    feat: enhance reboot logging with detailed reason and count tracking

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 5
-#define VERSION_PATCH 4
+#define VERSION_PATCH 5
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 295
-#define VERSION_DATE "2026-04-25"
-#define VERSION_TIME "07:07:32"
-#define VERSION "1.5.4.295"
-#define VERSION_STR "1.5.4.295 (2026-04-25 07:07:32)"
+#define VERSION_BUILD 297
+#define VERSION_DATE "2026-04-26"
+#define VERSION_TIME "08:17:51"
+#define VERSION "1.5.5.297"
+#define VERSION_STR "1.5.5.297 (2026-04-26 08:17:51)"
