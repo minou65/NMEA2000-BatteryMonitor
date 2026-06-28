bf4df6c0567aef32eb4804a167895cf291ddb74f
Andreas Zogg <andreas.zogg@swisscom.com>
2026-06-28 20:28:51 +0200

    refactor: remove unused battery-related variables from common.h

--- src/common.h
+++ src/common.h
@@ -28,23 +28,8 @@ extern bool debugMode;
 extern bool gParamsChanged;
 extern bool gSaveParams;
 
-//extern uint16_t gCapacityAh;
-//extern uint16_t gChargeEfficiencyPercent;
-//extern uint16_t gMinPercent;
-//extern uint16_t gTailCurrentmA;
-//extern uint16_t gFullVoltagemV;
-//extern uint16_t gFullDelayS;
-//extern float gShuntResistanceR;
-//extern uint16_t gMaxCurrentA;
 extern bool gSensorInitialized;
 
-//extern tN2kBatType gBatteryType;
-//extern tN2kBatNomVolt gBatteryVoltage;
-//extern tN2kBatChem gBatteryChemistry;
-//
-//extern float gVoltageCalibrationFactor;
-//extern float gCurrentCalibrationFactor;
-//extern float gCurrentThreshold;
 
 extern char gCustomName[64];
 extern char Version[];
